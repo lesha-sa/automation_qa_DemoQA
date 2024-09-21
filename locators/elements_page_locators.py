b93fb81e939abfdec4ea2973f830fdff7ce10063
Safrayter <leshkasa@gmail.com>
2024-09-21 16:29:23 +0300

    created elements_test / TextBox added elements_page.py, elements_page_locators.py, test_text_box.py, generator.py, data.py

---
+++ locators/elements_page_locators.py
@@ -0,0 +1,16 @@
+from selenium.webdriver.common.by import By
+
+
+class TextBoxPageLocators:
+
+    FULL_NAME = (By.CSS_SELECTOR, 'input[placeholder="Full Name"]')
+    EMAIL = (By.XPATH, '//*[@placeholder="name@example.com"]')
+    CURRENT_ADDRESS = (By.XPATH, '//*[@id = "currentAddress"]')
+    PERMANENT_ADDRESS = (By.XPATH, '//*[@id = "permanentAddress"]')
+    SUBMIT = (By.XPATH, '//*[@id = "submit"]')
+
+
+    CREATED_FULL_NAME = (By.CSS_SELECTOR, '#output #name')
+    CREATED_EMAIL = (By.CSS_SELECTOR, '#output #email')
+    CREATED_CURRENT_ADDRESS = (By.CSS_SELECTOR, '#output #currentAddress')
+    CREATED_PERMANENT_ADDRESS = (By.CSS_SELECTOR, '#output #permanentAddress')
---
+++ tests/elements_test/test_text_box.py
@@ -0,0 +1,18 @@
+import time
+from pages.elements_page import TextBoxPage
+from conftest import driver
+
+
+class TestElements:
+    class TestTextBox:
+
+        def test_text_box(self, driver):
+            text_box_page = TextBoxPage(driver, 'https://demoqa.com/text-box')
+            text_box_page.open()
+            text_box_page.fill_all_field()
+            full_name, email, current_address, permanent_address = text_box_page.check_filled_form()
+            output_name, output_email, output_cur_address, output_per_address = text_box_page.check_filled_form()
+            assert full_name == output_name, 'the full name does not match'
+            assert email == output_email, 'the email does not match'
+            assert current_address == output_cur_address, 'the current address does not match'
+            assert permanent_address == output_per_address, 'the permanent address does not match'

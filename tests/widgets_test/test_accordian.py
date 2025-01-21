9156c3325e1cb1c91175ed78c7913ea8f2f3cbee
Safrayter <leshkasa@gmail.com>
2025-01-22 00:54:23 +0300

    add negative message in assert

--- tests/widgets_test/test_accordian.py
+++ tests/widgets_test/test_accordian.py
@@ -11,6 +11,6 @@ class TestAccordingPage:
         first_title, first_content = accordian_page.check_accordian('first')
         second_title, second_content = accordian_page.check_accordian('second')
         third_title, third_content = accordian_page.check_accordian('third')
-        assert first_title == 'What is Lorem Ipsum?' and len(first_content) > 0
-        assert second_title == 'Where does it come from?' and len(second_content) > 0
-        assert third_title == 'Why do we use it?' and len(third_content) > 0
+        assert first_title == 'What is Lorem Ipsum?' and len(first_content) > 0, 'Incorrect title or missing text'
+        assert second_title == 'Where does it come from?' and len(second_content) > 0, 'Incorrect title or missing text'
+        assert third_title == 'Why do we use it?' and len(third_content) > 0, 'Incorrect title or missing text'
